346a4d5756002da6b49d4e2fd19df33c1382cc03
ToujoursTitou <104633833+ToujoursTitou@users.noreply.github.com>
2023-02-22 17:29:10 +0100

    Update main.c

--- SimpleCalculator/main.c
+++ SimpleCalculator/main.c
@@ -1,62 +1,67 @@
 #include "include.h"
-#include "headers.h"
+#include "header.h"
 #include "calcul.c"
 
 int main()
 {
-	int loop = 1;
-	while(loop == 1)
-	{	
-		double first_value = 0.0;
-		double second_value = 0.0;
-		double result = 0.0;
-		int operation = 0;
-		int input_loop = 1;
-		int input = 2;
-		printf("Enter operation : (addition : 1, substraction : 2, multiplication : 3, division : 4) :\n");
-		scanf("%d", &operation);
-		printf("Enter first value :\n");
-		scanf("%lf", &first_value);
-		printf("Enter second value :\n");
-		scanf("%lf", &second_value);
-		calcul(&operation, &first_value, &second_value, &result);
-		switch(operation)
-		{
-			case 1:
-				printf("Result of %f additioned with %f equal %f \n", first_value, second_value, result);
-				break;
-			case 2:
-				("Result of %f additioned with %f equal %f \n", first_value, second_value, result);	
-				break;
-			case 3:
-				printf("Result of %f multiplied with %f equal %f \n", first_value, second_value, result);
-				break
-			case 4:
-				printf("Result of %f divided with %f equal %f \n", first_value, second_value, result);	
-				break;
-			default:
-				printf("Error: try again!\n");
-				break;
-		}
-		while(input_loop == 1)
-		{	
-			printf("Want to do another operation ?(yes : 1/no : 0)\n");
-			scanf("%d", &input);
-			switch(input)
-			{
-				case 1:
-					printf("You choosed to do another operation\n");
-					printf("---------------------------------------\n");
-					input_loop = 0;
-					break;
-				case 0:
-					printf("You choosed to exit the program\n");
-					input_loop = 0;
-					loop = 0;
-					break;
-			}
-		}
-	}
-	return 0;
+	int* operation = NULL;
+       	int* calcul_loop = NULL;	
+	int* exit_loop = NULL;
+	double* first_value = NULL; 
+	double* second_value = NULL; 
+	double* result = NULL;
+	operation = malloc(sizeof(int));
+	calcul_loop = malloc(sizeof(int));
+	exit_loop = malloc(sizeof(int));
+	first_value = malloc(sizeof(double));
+	second_value = malloc(sizeof(double));
+	result = malloc(sizeof(double));
+	if(operation != NULL)
+		if(first_value != NULL)
+			if(second_value != NULL)
+				if(result != NULL)
+					if(calcul_loop != NULL)
+						if(exit_loop != NULL)
+							*calcul_loop = 1;
+							while(*calcul_loop == 1)
+							{
+								printf("Enter first value :\n");
+								scanf("%lf", first_value);
+								printf("Enter second value :\n");
+								scanf("%lf", second_value);
+								printf("Enter operation type(addition(1), substraction(2), multiplication(3), division(4)\n");
+								scanf("%d", operation);
+								calcul(operation, first_value, second_value, result);
+								printf("Result is : %f\n", *result);
+								*exit_loop = 1;
+								while(*exit_loop == 1)
+								{
+									printf("Press (1) to go back to the start of the loop, Press (0) to exit the program\n");
+									scanf("%d", operation);
+									switch(*operation)
+									{
+										case 1:
+											printf("Go back to the start of the loop...\n");
+											printf("-----------------------------------\n");
+											*exit_loop = 0;
+											*calcul_loop = 1;
+											break;
+										case 0:
+											printf("Exit the program...\n");
+											printf("-------------------\n");
+											*exit_loop = 0;
+											*calcul_loop = 0;
+											break;
+										default:
+											printf("Error: try again!\n");
+									}
+								}
+							}
+	free(operation);
+	free(calcul_loop);
+	free(exit_loop);
+	free(first_value);
+	free(second_value);
+	free(result);
+	return 0;	
 }
-
