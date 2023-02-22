26c1cc25b35a9afed3d6910c7dd1854134afd92a
ToujoursTitou <104633833+ToujoursTitou@users.noreply.github.com>
2023-02-22 17:27:48 +0100

    Update calcul.c

--- SimpleCalculator/calcul.c
+++ SimpleCalculator/calcul.c
@@ -1,17 +1,20 @@
-void calcul(int *operationPointer, double *firstValuePointer, double *secondValuePointer, double *resultPointer)
+void calcul(int *operationPointer, double *first_valuePointer, double *second_valuePointer, double *resultPointer)
 {
 	switch(*operationPointer)
 	{
 		case 1:
-			*resultPointer = *firstValuePointer + *secondValuePointer;
+			*resultPointer = *first_valuePointer + *second_valuePointer;
+			break;
 		case 2:
-			*resultPointer = *firstValuePointer - *secondValuePointer;
+			*resultPointer = *first_valuePointer - *second_valuePointer;
+			break;
 		case 3:
-			*resultPointer = *firstValuePointer * *secondValuePointer;
+			*resultPointer = *first_valuePointer * *second_valuePointer;
+			break;
 		case 4:
-			*resultPointer = *firstValuePointer / *secondValuePointer;
+			*resultPointer = *first_valuePointer / *second_valuePointer;
+			break;
 		default:
-			printf("Error!\n");
+			exit(0);
 	}
 }
-
